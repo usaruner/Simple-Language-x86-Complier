9791c343339863e06fd186bd0b66ba2765f07577
Henry Feng <henryf2@uci.edu>
2021-03-05 02:20:09 -0800

    Part 5: above 40/50

--- src/test/java/crux/CompilerStageTests.java
+++ src/test/java/crux/CompilerStageTests.java
@@ -194,7 +194,7 @@ final class CompilerStageTests {
             while ((length = inputStream.read(buffer)) != -1) {
                 result.write(buffer, 0, length);
             }
-            return result.toString(StandardCharsets.UTF_8);
+            return result.toString();
         }
     }
 
